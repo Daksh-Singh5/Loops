ea26dfb309aca511571b78b08a014b84d42214c4
Daksh-Singh5 <dakshsingh6612@gmail.com>
2024-12-19 21:30:22 +0530

    hello

---
+++ project.py
@@ -0,0 +1,7 @@
+median = int(input("Enter the median: "))
+exponent = int(input("Enter the exponent: "))
+answer= 0
+for i in range(1,exponent+1,1):
+    print(median,"X")
+
+print("=",median**exponent)
